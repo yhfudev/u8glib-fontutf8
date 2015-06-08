2e7212ed134e0654e5c56d744a9a88c786e8abd2
yhfudev <yhfudev@gmail.com>
2015-06-08 11:06:36 -0400

    remove old file

--- u8gapi/fontutf8-data.h
+++
@@ -1,6 +0,0 @@
-#include "fontutf8u8g.h"
-#include "fontpage_191.h"
-#define FONTDATA_ITEM(page, data) {page, NUM_ARRAY(data), data}
-const fontdata_t g_fontdata[] = {
-FONTDATA_ITEM(191, fontpage_191),
-};
